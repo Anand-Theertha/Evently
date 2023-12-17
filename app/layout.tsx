33c939bd687c7bef01134db686b25280f239d814
Anand-Theertha <54447234+Anand-Theertha@users.noreply.github.com>
2023-12-17 17:50:39 +0530

    feat:Clerk Authentication

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,27 +1,33 @@
-import type { Metadata } from 'next'
-import { Poppins } from 'next/font/google'
-import './globals.css'
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
 
-const poppins = Poppins({ subsets: ['latin'],
-weight: ['400' , '500' , '600' , '700'],
-variable: '--font-poppins' })
+import "./globals.css";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+  variable: "--font-poppins",
+});
 
 export const metadata: Metadata = {
-  title: 'Evently',
-  description: 'Platform for event management.',
+  title: "Evently",
+  description: "Platform for event management.",
   icons: {
-    icon: 'assets/images/logo.svg'
-  }
-}
+    icon: "assets/images/logo.svg",
+  },
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={poppins.className}>{children}</body>
-    </html>
-  )
+    <ClerkProvider>
+      <html lang="en">
+        <body className={poppins.className}>{children}</body>
+      </html>
+    </ClerkProvider>
+  );
 }
