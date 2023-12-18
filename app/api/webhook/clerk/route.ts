8d1c3849685be282329541923249f0408907e166
Anand-Theertha <54447234+Anand-Theertha@users.noreply.github.com>
2023-12-18 15:37:52 +0530

    database models: category & order

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -1,7 +1,7 @@
 import { Webhook } from "svix";
 import { headers } from "next/headers";
 import { WebhookEvent } from "@clerk/nextjs/server";
-import { createUser } from "@/lib/actions/user.actions";
+import { createUser, deleteUser, updateUser } from "@/lib/actions/user.actions";
 import { clerkClient } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
 
@@ -90,7 +90,7 @@ export async function POST(req: Request) {
       photo: image_url,
     }
 
-    // const updatedUser = await updateUser(id, user)
+    const updatedUser = await updateUser(id, user)
 
     return NextResponse.json({ message: 'OK', user: updatedUser })
   }
@@ -98,7 +98,7 @@ export async function POST(req: Request) {
   if (eventType === 'user.deleted') {
     const { id } = evt.data
 
-    // const deletedUser = await deleteUser(id!)
+    const deletedUser = await deleteUser(id!)
 
     return NextResponse.json({ message: 'OK', user: deletedUser })
   }
